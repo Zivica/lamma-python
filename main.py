1dfb03691d30c284aa14ec7151a94a7e2213d41f
Jankovic Mihajlo <jankomikaacer@gmail.com>
2026-03-03 00:25:50 +0100

    Lesson 3 - Functional response from the Gemini 2.5

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+client = genai.Client()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
 def main():
-    print("Hello from lamma-python!")
+    if api_key is None:
+        raise Exception("API key not set")
+    response = client.models.generate_content(model="gemini-2.5-flash",
+                                              contents="Why is Boot.dev such a great place "
+                                                       "to learn backend development?"
+                                                       " Use one paragraph maximum.")
+    print(response.text)
 
 
 if __name__ == "__main__":
