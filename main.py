c24fa477d615d15b6e0e1ba0aac86e2cc663f9d1
Jankovic Mihajlo <jankomikaacer@gmail.com>
2026-03-04 21:08:30 +0100

    Lesson 3 again

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import os
 from dotenv import load_dotenv
 from google import genai
 
-client = genai.Client()
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
+client = genai.Client(api_key=api_key)
 
 def main():
     if api_key is None:
         raise Exception("API key not set")
     response = client.models.generate_content(model="gemini-2.5-flash",
-                                              contents="Why is Boot.dev such a great place "
-                                                       "to learn backend development?"
-                                                       " Use one paragraph maximum.")
+                                              contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
     print(response.text)
 
 
